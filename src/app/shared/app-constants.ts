79ac6628b1f24f200d5a0744650e6330031001da
Diogo Castro <ctw00156@criticaltechworks.com>
2018-12-17 09:34:23 +0000

    fixed BE uri

--- src/app/shared/app-constants.ts
+++ src/app/shared/app-constants.ts
@@ -1,3 +1,3 @@
-export const APP_URI = 'http://localhost:8080/books/ctw';
+export const APP_URI = 'http://payara4-ispa-cloud-int.mp-dev-cnap.bmwgroup.net/books/ctw';
 
 //export const APP_DB = 'http://localhost:3000';
